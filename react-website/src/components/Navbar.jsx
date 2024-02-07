0ae33b6f811164654492b98602d1b3049c8592aa
Alex Whalen <awhalen1999@Gmail.com>
2024-02-07 16:58:26 -0330

    nav bar dropdown

--- react-website/src/components/Navbar.jsx
+++ react-website/src/components/Navbar.jsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import { SlMenu, SlArrowUp } from 'react-icons/sl';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav>
+      <div>
+        <a href='#top'>
+          <img src='http://via.placeholder.com/100x100' alt='Company Logo' />
+        </a>
+      </div>
+      <div>
+        <button onClick={toggleMenu}>
+          {isOpen ? <SlArrowUp size={30} /> : <SlMenu size={30} />}
+        </button>
+        {isOpen && (
+          <div>
+            <ul>
+              <li>
+                <Link to='about' smooth={true} duration={1000}>
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to='why' smooth={true} duration={1000}>
+                  Why
+                </Link>
+              </li>
+              <li>
+                <Link to='steps' smooth={true} duration={1000}>
+                  Steps
+                </Link>
+              </li>
+              <li>
+                <Link to='services' smooth={true} duration={1000}>
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link to='contact' smooth={true} duration={1000}>
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
