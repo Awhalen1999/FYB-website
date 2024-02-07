354a059aebebced77496e6bec53224cfcdc1f254
Alex Whalen <awhalen1999@Gmail.com>
2024-02-07 17:21:49 -0330

    nav bar change to dropdown menu @ 768px

--- react-website/src/components/Navbar.jsx
+++ react-website/src/components/Navbar.jsx
@@ -11,17 +11,47 @@ const Navbar = () => {
 
   return (
     <nav>
+      {' '}
       <div>
         <a href='#top'>
           <img src='http://via.placeholder.com/100x100' alt='Company Logo' />
         </a>
       </div>
-      <div>
-        <button onClick={toggleMenu}>
+      <div className='navbar'>
+        <ul>
+          <li>
+            <Link to='about' smooth={true} duration={1000}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to='why' smooth={true} duration={1000}>
+              Why
+            </Link>
+          </li>
+          <li>
+            <Link to='steps' smooth={true} duration={1000}>
+              Steps
+            </Link>
+          </li>
+          <li>
+            <Link to='services' smooth={true} duration={1000}>
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to='contact' smooth={true} duration={1000}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className='dropdown'>
+        <button className='nav-button' onClick={toggleMenu}>
           {isOpen ? <SlArrowUp size={30} /> : <SlMenu size={30} />}
         </button>
         {isOpen && (
-          <div>
+          <div className='dropdown-menu'>
             <ul>
               <li>
                 <Link to='about' smooth={true} duration={1000}>
