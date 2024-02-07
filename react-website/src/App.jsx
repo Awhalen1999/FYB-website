3017df8302221e040740ce2110cd6d906f8b8e5a
Alex Whalen <awhalen1999@Gmail.com>
2024-02-07 15:34:01 -0330

    import react icons and react scroll

--- react-website/src/App.jsx
+++ react-website/src/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
-      <p>
-        He wandered down the stairs and into the basement. The damp, musty smell
-        of hung in the air. A single, small window let in a glimmer of light,
-        but this simply made the shadows in the basement deeper. He inhaled
-        deeply and looked around at a mess that had been accumulating for over
-        25 years. He was positive that this was the place he wanted to live.
-      </p>
+      <Navbar />
     </div>
   );
 };
