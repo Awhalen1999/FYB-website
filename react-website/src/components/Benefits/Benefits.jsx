c2be4c55641d395fc79a370b5d6e6814c6efc776
Alex Whalen <awhalen1999@Gmail.com>
2024-02-08 19:21:20 -0330

    benefits component added

--- react-website/src/components/Benefits/Benefits.jsx
+++ react-website/src/components/Benefits/Benefits.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Benefits.css';
+import { TfiCheckBox } from 'react-icons/tfi';
 
 const Benefits = () => {
-  return <div>Benefits</div>;
+  const [isVisible, setIsVisible] = useState(false);
+
+  const sections = [
+    'Reduce pain and soreness',
+    'Decrease muscle / myofascial tension',
+    'Improve circulation & organ function',
+  ];
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 25) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  return (
+    <div className='item-box'>
+      <div className={`item-section-container ${isVisible ? 'visible' : ''}`}>
+        {sections.map((section, index) => (
+          <div key={index} className='item-section'>
+            <div className='item-content'>
+              <TfiCheckBox size={40} />
+              <p>{section}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className='learn-more-button'>LEARN MORE</button>
+    </div>
+  );
 };
 
 export default Benefits;
--- react-website/src/components/Hero/Hero.jsx
+++ react-website/src/components/Hero/Hero.jsx
@@ -9,9 +9,9 @@ const Hero = () => {
         <h1 className='hero-title-1'>Relieve pain</h1>
         <h1 className='hero-title-2'>Embrace life's adventures</h1>
         <p className='hero-subtitle'>
-          With Myofascial Release Registered Massage Therapy
+          With myofascial Release Registered Massage Therapy
         </p>
-        <button className='hero-button'>Placeholder</button>
+        <button className='hero-button'>BOOK AN APPOINTMENT</button>
       </div>
     </div>
   );
