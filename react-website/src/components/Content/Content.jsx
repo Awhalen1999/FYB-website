2efe9576035fd605d6c25f8ee888bb4dc94eb55c
Alex Whalen <awhalen1999@Gmail.com>
2024-02-09 18:08:30 -0330

    added content component

--- react-website/src/components/Content/Content.jsx
+++ react-website/src/components/Content/Content.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import './Content.css';
+import { TfiCheckBox } from 'react-icons/tfi';
+
+const Section = ({ text }) => (
+  <div className='content-section'>
+    <div className='icon-wrapper'>
+      <TfiCheckBox size={35} />
+    </div>
+    <p>{text}</p>
+  </div>
+);
 
 const Body = () => {
-  return <div>Body</div>;
+  const sections = [
+    [
+      "Your body's natural healing process will get a boost",
+      'Experience an improved range of motion',
+      'Stress and tension decreases',
+    ],
+    [
+      'Enjoy an improved sense of wellbeing',
+      "Your body's blood flow increases",
+      'Pain and inflammation reduces',
+    ],
+  ];
+
+  return (
+    <div className='content'>
+      <p className='content-main'>
+        <strong>Myofascial release massage therapy</strong> is a specialized
+        form of massage that focuses on the fascia, a connective tissue that
+        surrounds and supports muscles, bones, and organs throughout the body.
+        The fascia can become tight, restricted, or damaged due to factors such
+        as stress, injury, poor posture, or inflammation, leading to pain and
+        discomfort.
+      </p>
+      <div className='sections-wrapper'>
+        {sections.map((group, index) => (
+          <div key={index} className='content-group'>
+            {group.map((section, index) => (
+              <Section key={index} text={section} />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
