7f1e73d5eb0c14f8d96788b6fb52b05e9b79ae0c
Alex Whalen <awhalen1999@Gmail.com>
2024-02-12 15:55:27 -0330

    added carousel install and changed component formatting

--- react-website/src/components/Services/Services.jsx
+++ react-website/src/components/Services/Services.jsx
@@ -1,33 +1,25 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './Services.css';
-import placeholderImage from '../../assets/placeholder.png';
+import ServicesImage from '../../assets/FindYourBalance-Logo.png';
 
 const ServiceCard = ({ title }) => (
   <div className='service-card'>
-    <img src={placeholderImage} alt={title} className='service-card-image' />
-    <h3 className='service-card-title'>{title}</h3>
+    <img src={ServicesImage} alt={title} className='service-card-image' />
+    <p className='service-card-text'>Placeholder text for {title}</p>
+    <button className='service-card-button'>BOOK NOW</button>
   </div>
 );
 
-const Services = () => (
-  <div id='Services' className='services'>
-    <h1>Services</h1>
-    <Carousel
-      showThumbs={false}
-      showStatus={false}
-      showIndicators={false}
-      centerMode
-      centerSlidePercentage={50}
-    >
-      <ServiceCard title='Service 1' />
-      <ServiceCard title='Service 2' />
-      <ServiceCard title='Service 3' />
-      <ServiceCard title='Service 4' />
-      <ServiceCard title='Service 5' />
-    </Carousel>
-  </div>
-);
+const Services = () => {
+  return (
+    <div id='Services' className='services'>
+      <h1>Services</h1>
+      <div className='cards-container'>
+        <ServiceCard title='Service 1' />
+        <ServiceCard title='Service 2' />
+      </div>
+    </div>
+  );
+};
 
 export default Services;
