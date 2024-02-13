86c5bb722f14d0f7e5f32ef736d4a62d3ad63d99
Alex Whalen <awhalen1999@Gmail.com>
2024-02-13 15:11:34 -0330

    services component

--- react-website/src/components/Services/Services.jsx
+++ react-website/src/components/Services/Services.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import './Services.css';
-import ServicesImage from '../../assets/FindYourBalance-Logo.png';
-
+import ServicesImage from '../../assets/massage.png';
 const ServiceCard = ({ title }) => (
   <div className='service-card'>
     <img src={ServicesImage} alt={title} className='service-card-image' />
-    <p className='service-card-text'>Placeholder text for {title}</p>
+    <h2 className='service-card-title'>{title}</h2>
+    <p className='service-card-text'>
+      Enhance your body's natural healing process, feel the difference as your
+      range of motion improves, stress and tension decrease, and your overall
+      sense of well-being is elevated. Experience increased blood flow, reduced
+      pain, and inflammation
+    </p>
     <button className='service-card-button'>BOOK NOW</button>
   </div>
 );
-
 const Services = () => {
   return (
     <div id='Services' className='services'>
       <h1>Services</h1>
       <div className='cards-container'>
-        <ServiceCard title='Service 1' />
-        <ServiceCard title='Service 2' />
+        <ServiceCard title='Registered Massage Therapy/Specializing in Myofascial Release' />
       </div>
     </div>
   );
 };
-
 export default Services;
