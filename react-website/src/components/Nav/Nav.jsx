fd7bb6ead8b4cd84f36978294305d0be90602b56
Alex Whalen <awhalen1999@Gmail.com>
2024-02-13 16:30:44 -0330

    nav component and hero image changed

--- react-website/src/components/Nav/Nav.jsx
+++ react-website/src/components/Nav/Nav.jsx
@@ -6,15 +6,36 @@ import logoImage from '../../assets/FindYourBalance-LogoPlusText-BG-Remove-2.png
 
 const MenuItem = ({ item }) => (
   <li>
-    <Link to={item} smooth={true} duration={1000}>
+    <Link to={item} smooth duration={1000}>
       {item}
     </Link>
   </li>
 );
 
+const DesktopMenu = ({ menuItems }) => (
+  <ul className='nav-dropdown desktop-menu'>
+    {menuItems.map((item, index) => (
+      <MenuItem key={index} item={item} />
+    ))}
+  </ul>
+);
+
+const MobileMenu = ({ menuItems, isOpen, setIsOpen }) => (
+  <div className='mobile-menu'>
+    <button className='nav-button' onClick={() => setIsOpen(!isOpen)}>
+      {isOpen ? <SlArrowUp size='3em' /> : <SlMenu size='3em' />}
+    </button>
+    <ul className={`nav-dropdown ${isOpen ? 'open' : ''}`}>
+      {menuItems.map((item, index) => (
+        <MenuItem key={index} item={item} />
+      ))}
+    </ul>
+  </div>
+);
+
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const menuItems = ['About', 'Learn more', 'Services', 'Contact me']; // replace with your section IDs
+  const menuItems = ['About', 'Learn more', 'Services', 'Contact me'];
 
   return (
     <nav className='nav-container'>
@@ -23,17 +44,13 @@ const Nav = () => {
           <img className='logo' src={logoImage} alt='Company Logo' />
         </a>
       </div>
-      <div>
-        <button className='nav-button' onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? <SlArrowUp size='3em' /> : <SlMenu size='3em' />}
-        </button>
-        {isOpen && (
-          <ul className='nav-dropdown'>
-            {menuItems.map((item, index) => (
-              <MenuItem key={index} item={item} />
-            ))}
-          </ul>
-        )}
+      <div className='menu-container'>
+        <DesktopMenu menuItems={menuItems} />
+        <MobileMenu
+          menuItems={menuItems}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
       </div>
     </nav>
   );
